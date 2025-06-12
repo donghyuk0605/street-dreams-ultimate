7ca399702d0dee43c7c7afc4ba2d5f4775ad9d79
김동혁 <93080622+donghyuk0605@users.noreply.github.com>
2025-06-13 02:24:12 +0900

    fix phaser import for next

--- components/phaser-demo.tsx
+++ components/phaser-demo.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useRef } from "react"
-import Phaser from "phaser"
+import * as Phaser from "phaser"
 
 export default function PhaserDemo() {
   const containerRef = useRef<HTMLDivElement>(null)
